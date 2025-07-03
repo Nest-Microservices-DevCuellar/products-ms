34ef7911d0eae7bbd1237cfc528a2bb1dc4e4223
David Cuéllar <da@goldensoft.com>
2025-07-03 14:26:18 +0200

    Mejoras en el manejo de errores con RpcException

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from 'generated/prisma';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -43,7 +44,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     const product = await this.product.findFirst({ where: { id, available: true } });
 
     if (!product) {
-      throw new NotFoundException(`Product with id #${id} not found`)
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.NOT_FOUND
+      })
     }
 
     return product;
